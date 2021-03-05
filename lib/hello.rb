7a438f92355a258ba220cfe9c972c007dc752937
Milan <milanvpage@gmail.com>
2021-03-05 15:09:13 -0800

    commit tree 52d802c8c21eeacca23b2a650291a457fabeceda

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
+def hello_t(array)
+    if block_given?
+    i = 0
 
-end
+    while i < array.length
+        yield(array[i])
+        i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end 
 
 # call your method here!
+#"Jim"]
+#hello_t(names){ |name| puts name }
 
+#other_names = ["ALI","JASMINE","PERSEPHONE"]
+#hello_t(other_names){ |name| puts name.upcase }
